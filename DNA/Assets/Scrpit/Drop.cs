77cf81cbf81a2d56ffa44d07045575d59f227331
Lianke <1026193951@sjtu.edu.cn>
2018-05-26 10:48:47 +0800

    demo
    目前直接从菜单拖出的核苷酸不能准确放置到鼠标松开的位置，但在空间内靠鼠标点击拖动物体的功能正常

---
+++ DNA/Assets/Scrpit/Drop.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Drop : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDropHandler
+{
+    //鼠标进入  
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //有拖拽物，drop区域变色，否则return  
+        GameObject dragobj = eventData.pointerDrag;
+
+        if (dragobj == null) return;
+
+        GetComponent<Image>().color = Color.green;
+    }
+
+    //鼠标离开 重置颜色white  
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Image>().color = Color.white;
+    }
+
+    //drop 赋予拖拽物体sprite  
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject dragobj = eventData.pointerDrag;
+
+        // if (dragobj == null) return;  
+
+        GetComponent<Image>().sprite = dragobj.GetComponent<Image>().sprite;
+
+        GetComponent<Image>().color = Color.white;
+    }
+}
---
+++ DNA/Assets/Scrpit/Tool.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class Tool : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    //生成的icon  
+    private GameObject draged_icon;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        //设置icon属性，添加组件  
+        Debug.Log(this.name);
+ 
+        draged_icon = new GameObject("icon");
+        draged_icon.transform.localScale /= 5;
+
+        draged_icon.transform.SetParent(GameObject.Find("Canvas").transform, false);
+        
+        draged_icon.AddComponent<RectTransform>();
+        draged_icon.AddComponent<Image>();
+        draged_icon.GetComponent<Image>().sprite = GetComponent<Image>().sprite;
+        
+
+        //让图标不执行事件检测，防止icon妨碍后面的event system  
+        CanvasGroup group = draged_icon.AddComponent<CanvasGroup>();
+        group.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //在RectTransform下 实现鼠标 物体的跟随效果  
+        Vector3 worldpos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draged_icon.GetComponent<RectTransform>(), eventData.position, Camera.main, out worldpos))
+        {
+            draged_icon.transform.position = worldpos;
+        }
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+        if (draged_icon != null)
+        {
+            GameObject tmp = new GameObject();
+            switch (this.name)
+            {
+                case "A":
+                    tmp = (GameObject)Resources.Load("Prefabs/A");
+                    break;
+                case "C":
+                    tmp = (GameObject)Resources.Load("Prefabs/C");
+                    break;
+                case "G":
+                    tmp = (GameObject)Resources.Load("Prefabs/G");
+                    break;
+                case "T":
+                    tmp = (GameObject)Resources.Load("Prefabs/T");
+                    break;
+                default:
+                    break;
+            }
+            
+            
+            Instantiate(tmp);
+            tmp.transform.position = draged_icon.transform.position;
+            
+            Destroy(draged_icon.gameObject);
+        }
+        
+    }
+}
